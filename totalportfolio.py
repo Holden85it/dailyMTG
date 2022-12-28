b0f01169a78dfb133faa857f3c48f83508c6ce8d
Holden85it <holden_85@hotmail.it>
2022-12-28 22:05:09 +0000

    first commit

---
+++ totalportfolio.py
@@ -0,0 +1,140 @@
+import csv
+import pandas as pd
+import os
+import datetime
+import requests
+
+api_key = "738beaa5-ec04-4767-8a84-5509e5afb6da"
+api_endpoint = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"
+
+# Set the request headers
+headers = {
+    "X-CMC_PRO_API_KEY": api_key
+}
+
+# Set the currencies and currencies to convert to
+currencies = ["BTC", "ETH", "ADA", "DOGE", "ATOM", "DOT", "LTC", "XLM", "XRP", "XMR",  "BCH", "MATIC", "SOL"]
+convert_to = ["GBP"]
+
+# Set the request parameters
+params = {
+  "symbol": ",".join(currencies),
+  "convert": ",".join(convert_to)
+}
+
+# Send the request to the API endpoint
+response = requests.get(api_endpoint, params=params, headers=headers)
+
+# Parse the response JSON
+response_data = response.json()
+
+# Create a list to store the rows of the CSV file
+rows = []
+
+# Loop through each currency and each currency to convert to
+for currency in currencies:
+  for convert in convert_to:
+    # Get the price of the currency in the specified currency
+    price = response_data["data"][currency]["quote"][convert]["price"]
+
+    # Add a new row to the list with the currency, currency to convert to, and price
+    rows.append([currency, convert, price])
+
+# Open the CSV file for writing, overwriting any existing file
+with open("currentprices.csv", "w", newline="") as csvfile:
+  # Create a CSV writer
+  writer = csv.writer(csvfile)
+
+  # Write the header row
+  writer.writerow(["Currency", "Convert To", "Price"])
+
+  # Write the rows with the prices
+  writer.writerows(rows)
+
+# Read the contents of the currentprices.csv and quantities.csv files
+# into separate dataframes using pandas
+current_prices_df = pd.read_csv("currentprices.csv")
+quantities_df = pd.read_csv("quantities.csv")
+
+# Loop through the rows of the quantities dataframe and calculate the
+# total value of each currency in your portfolio
+current_value = 0
+for index, row in quantities_df.iterrows():
+    currency = row["Currency"]
+    quantity = row["Quantity"]
+    # Look up the current price of the currency in the current_prices dataframe
+    # but only pick up the price if the "Convert To" column is "GBP"
+    price = current_prices_df[(current_prices_df["Currency"] == currency) & (current_prices_df["Convert To"] == "GBP")]["Price"].values[0]
+    # Calculate the total value of the currency in your portfolio
+    value = price * quantity
+    current_value += value
+
+# Print the total value of your portfolio
+print("Total value of portfolio: ", int(round(current_value,0)))
+
+def load_csv(filename):
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            reader = csv.DictReader(file)
+            return list(reader)
+    else:
+        return []
+
+def calculate_pnl(history, current_value):
+    previous_value = float(history[-1]["Value"])
+    difference = current_value - previous_value
+    if difference > 0:
+        return "Profit: {}".format(int(round(difference,0)))
+    else:
+        return "Loss: {}".format(int(round(difference,0)))
+
+def append_to_csv(filename, data, new_data):
+    with open(filename, "a") as file:
+        fieldnames = data[0].keys()
+        writer = csv.DictWriter(file, fieldnames=fieldnames, lineterminator="\n")
+        writer.writerow(new_data)
+        file.close()
+
+# Get the current working directory and construct the full path to the CSV file
+cwd = os.getcwd()
+filename = os.path.join(cwd, "portfolioHistory.csv")
+
+# Load the data from the CSV file
+data = load_csv(filename)
+
+# Calculate the profit and loss
+pnl = calculate_pnl(data, current_value)
+print(pnl)
+
+# Get the current date and format it using the same format as the dates in the CSV file
+current_date = datetime.datetime.now().strftime("%d-%b-%Y")
+
+# Append the current value to the CSV file
+new_data = {"Date": current_date, "Value": int(round(current_value,0))}
+append_to_csv(filename, data, new_data)
+
+
+# Set the Pushbullet API endpoint and your API key
+ENDPOINT = "https://api.pushbullet.com/v2/pushes"
+API_KEY = "o.52oPsw0YJky0iG8Xwksnk7VR32SDx1yy"
+
+# Set the parameters for the API request
+params = {
+    "type": "note",
+    "title": "DailyPnl",
+    "body": pnl
+}
+
+# Set the headers for the API request
+headers = {
+    "Access-Token": API_KEY,
+    "Content-Type": "application/json"
+}
+
+# Make the API request
+response = requests.post(ENDPOINT, json=params, headers=headers)
+
+# Parse the response data
+data = response.json()
+
+
